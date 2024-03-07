1ac5264bdc74533fb00a467b747e63f937adafd6
NatanCristhoffe <natan.cristhoffe@outlook.com>
2024-03-07 19:40:11 -0300

    Tela de longin pronta

--- contact/forms.py
+++ contact/forms.py
@@ -1,13 +1,17 @@
 from django import forms
 from django.core.exceptions import ValidationError
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from contact.models import *
 
 class ContactForm(forms.ModelForm):
+    picture = forms.ImageField(widget=forms.FileInput(attrs={'accept':'image/*',}))
     class Meta:
         model = Contact
         fields = (
             'first_name', 'last_name', 'phone',
             'email', 'description', 'category',
+            'picture',
         )
     def clean(self):
 
@@ -35,3 +39,32 @@ class ContactForm(forms.ModelForm):
                     )
                 )
             return first_name
+    
+class RegisterForm(UserCreationForm):
+    first_name = forms.CharField(
+        required=True,
+        min_length=3
+    )
+    last_name = forms.CharField(
+        required=True,
+        min_length=3
+    )
+    email = forms.EmailField(
+        required=True,
+        min_length=4
+    )
+    class Meta:
+        model = User
+        fields = (
+            'first_name', 'last_name', 'email',
+            'username', 'password1', 'password2',
+        )
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            self.add_error(
+                'email',
+                ValidationError('Este e-mail foi cadastrado por outro usuario!', code='Invalid')
+            )
+
+        return email
---
+++ contact/views/user_forms.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render, redirect
+from contact.forms import RegisterForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import auth
+from django.contrib import messages
+
+def register(request):
+    form = RegisterForm()
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Usuário registrado com sucesso.')
+            return redirect("contact:login")
+        
+    return render(
+        request,
+        'contact/register.html',
+        {
+            'form':form
+        }
+    )
+
+def login_view(request):
+    form = AuthenticationForm(request,data=request.POST)
+    
+    if form.is_valid():
+        user = form.get_user()
+        messages.success(request,"Login realizado com sucesso!")
+        auth.login(request,user)
+        return redirect("contact:index")
+    messages.error(request,"Login inválido!")
+    
+    return render(
+        request,
+        'contact/login.html',
+        {
+            'form':form
+        }
+    )
+
+def logout_view(request):
+    auth.logout(request)
+    return redirect('contact:login')
