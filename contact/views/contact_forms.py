f3a146800fe2497a7e36ed3950cc2c19301172c1
NatanCristhoffe <natan.cristhoffe@outlook.com>
2024-03-09 13:30:35 -0300

    Projeto Agenda completo

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -2,8 +2,9 @@ from django.shortcuts import render, get_object_or_404, redirect
 from contact.forms import *
 from django.urls import reverse
 from contact.models import Contact
+from django.contrib.auth.decorators import login_required
 
-
+@login_required(login_url='contact:login')
 def create(request):
     form_action = reverse("contact:create")
     if request.method == 'POST':
@@ -15,7 +16,9 @@ def create(request):
 
         }
         if form.is_valid():
-            contact = form.save()
+            contact = form.save(commit=False)
+            contact.owner = request.user
+            contact.save()
             return redirect('contact:update', contact_id=contact.pk)
         return render(
             request,
@@ -29,13 +32,13 @@ def create(request):
     return render(
         request,
         'contact/create.html',
-        context
+        context,
     )
 
 
-
+@login_required(login_url='contact:login')
 def update(request, contact_id):
-    contact = get_object_or_404(Contact, pk=contact_id, show=True)
+    contact = get_object_or_404(Contact, pk=contact_id, show=True, owner=request.user)
     form_action = reverse("contact:update", args=(contact_id,))
     if request.method == 'POST':
         form = ContactForm(request.POST, request.FILES, instance=contact)
@@ -62,9 +65,9 @@ def update(request, contact_id):
         'contact/create.html',
         context
     )
-
+@login_required(login_url='contact:login')
 def delete(request, contact_id):
-    contact = get_object_or_404(Contact, pk=contact_id, show=True)
+    contact = get_object_or_404(Contact, pk=contact_id, show=True, owner=request.user)
     confirmation = request.POST.get('confirmation', 'no')
 
     if confirmation == 'yes':
--- contact/views/user_forms.py
+++ contact/views/user_forms.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect
-from contact.forms import RegisterForm
+from contact.forms import RegisterForm, RegisterUpdateForm
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib import auth
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 def register(request):
     form = RegisterForm()
@@ -20,6 +21,29 @@ def register(request):
             'form':form
         }
     )
+@login_required(login_url='contact:login')
+def user_update(request):
+    form = RegisterUpdateForm()
+    if request.method != "POST":
+        return render(
+            request,
+            'contact/user-form.html',
+            {
+                'form':form
+            }
+        )
+    form = RegisterUpdateForm(data=request.POST, instance=request.user)
+
+    if not form.is_valid():
+        return render(
+            request,
+            'contact/user-form.html',
+            {
+                'form':form
+            }
+        )
+    form.save()
+    return redirect('contact:user_update')
 
 def login_view(request):
     form = AuthenticationForm(request,data=request.POST)
@@ -38,7 +62,7 @@ def login_view(request):
             'form':form
         }
     )
-
+@login_required(login_url='contact:login')
 def logout_view(request):
     auth.logout(request)
     return redirect('contact:login')
